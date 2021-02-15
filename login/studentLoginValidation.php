421bcfb4a5d9e89e1f5d5f6c6d209a790ea7b6ca
AndrewF27 <64434343+AndrewF27@users.noreply.github.com>
2021-02-15 12:00:54 -0800

    added/ worked on some pages

---
+++ login/studentLoginValidation.php
@@ -0,0 +1,28 @@
+<?php
+$email = $_POST["teacherEmail"];
+$pass = $_POST["teacherPass"];
+ini_set('display_errors', 1);
+
+//Attempts to connect to database using mysqli
+//Still need to figure out database location
+$connection = new mysqli ("mysql.retentionapp.club","retentionadmin","R3t3n@dm1n");
+
+//Check connection to database
+if ($connection -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $connection -> connect_error;
+    exit();
+  }
+ //select database
+  mysqli_select_db($connection,"retentionapp_login");
+
+$result = mysqli_query($connection, "SELECT * FROM teacher_login WHERE teacherEmail = '$email' and teacherPass = '$pass'");
+
+$rows = mysqli_num_rows($result);
+if($rows == 1) {
+    echo"Login Successful.";
+}
+else{
+    echo"Sorry, login failed. Please try again.";
+}
+    
+?>
