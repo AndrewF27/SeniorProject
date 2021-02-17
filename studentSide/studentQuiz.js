ca0aa2ff37d2636dc8a1bfdb22c6488516e6199d
AndrewF27 <64434343+AndrewF27@users.noreply.github.com>
2021-02-17 14:56:41 -0800

    put things in functions

--- studentSide/studentQuiz.js
+++ studentSide/studentQuiz.js
@@ -9,29 +9,38 @@ var testTextSplit = testText.split(" ");
 //document.write(testTextSplit);
 
 //String Sifter
-var randomWord = testTextSplit[Math.floor(Math.random()*testTextSplit.length)];
-document.write(randomWord);
+function questionGen() {
+    var randomWord = testTextSplit[Math.floor(Math.random()*testTextSplit.length)];
+//document.write(randomWord);
 /*var randomWordCount = (testText.match(randomWord).length)
 document.write(randomWordCount)*/
-var randomWordCount = 0;
-for (i = 0; i < testTextSplit.length; i++) {
-    if (testTextSplit[i] == randomWord) {
-        randomWordCount = randomWordCount + 1;
+    var randomWordCount = 0;
+    for (i = 0; i < testTextSplit.length; i++) {
+        if (testTextSplit[i] == randomWord) {
+            randomWordCount = randomWordCount + 1;
+        }
     }
 }
+
+questionGen();
 //document.write(randomWordCount);
 //document.getElementById("question").innerHTML = randomWord;
 //Question & Answer Generation
-var question1 = "How many times does the word ";
-var question2 = " appear in the test text?";
-var questionTotal = question1.concat(randomWord, question2);
-document.getElementById("after").innerHTML = questionTotal;
 
-var answer1 = "The word "
-var answer2 = " appears "
-var answer3 = " time(s)!"
-var answerTotal = answer1.concat(randomWord, answer2, randomWordCount, answer3);
-document.getElementById("test").innerHTML = answerTotal;
+function answerGen() {
+    var question1 = "How many times does the word ";
+    var question2 = " appear in the test text?";
+    var questionTotal = question1.concat(randomWord, question2);
+    document.getElementById("after").innerHTML = questionTotal;
+
+    var answer1 = "The word "
+    var answer2 = " appears "
+    var answer3 = " time(s)!"
+    var answerTotal = answer1.concat(randomWord, answer2, randomWordCount, answer3);
+    document.getElementById("test").innerHTML = answerTotal;
+}
+
+answerGen();
 
 //Awarding Points
 var studentResponse = 0;
@@ -41,10 +50,12 @@ if (studentResponse == randomWordCount) {
     score++;
 }
 //Page Refreshing
-/*
+
 var quizActive = 1;
 
-while (quizActive != 1) {
-    location.reload()
+
+function checkQuiz() {
+    while (quizActive != 1) {
+        location.reload();
+    }
 }
-*/
