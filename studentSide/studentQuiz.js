6e26e2b0d89b7503b6fe0bb3baf8e62e8e965c93
Rasheed Adam <rasheedadamh@gmail.com>
2021-02-18 10:43:24 -0800

    added stuff from test js file

--- studentSide/studentQuiz.js
+++ studentSide/studentQuiz.js
@@ -9,41 +9,29 @@ var testTextSplit = testText.split(" ");
 //document.write(testTextSplit);
 
 //String Sifter
-function questionGen() {
-    var randomWord = testTextSplit[Math.floor(Math.random()*testTextSplit.length)];
-//document.write(randomWord);
+var randomWord = testTextSplit[Math.floor(Math.random()*testTextSplit.length)];
+document.write(randomWord);
 /*var randomWordCount = (testText.match(randomWord).length)
 document.write(randomWordCount)*/
-    var randomWordCount = 0;
-    for (i = 0; i < testTextSplit.length; i++) {
-        if (testTextSplit[i] == randomWord) {
-            randomWordCount = randomWordCount + 1;
-        }
+var randomWordCount = 0;
+for (i = 0; i < testTextSplit.length; i++) {
+    if (testTextSplit[i] == randomWord) {
+        randomWordCount = randomWordCount + 1;
     }
 }
-
-questionGen();
 //document.write(randomWordCount);
 //document.getElementById("question").innerHTML = randomWord;
 //Question & Answer Generation
+var question1 = "How many times does the word ";
+var question2 = " appear in the test text?";
+var questionTotal = question1.concat(randomWord, question2);
+document.getElementById("after").innerHTML = questionTotal;
 
-function button() {
-var answer = prompt("");
-var answer1 = "Correct! The word "
+var answer1 = "The word "
 var answer2 = " appears "
 var answer3 = " time(s)!"
-var answer4 = "Incorrect! The word "
 var answerTotal = answer1.concat(randomWord, answer2, randomWordCount, answer3);
-var answerTotal2 = answer4.concat(randomWord, answer2, randomWordCount, answer3);
-if (answer == randomWordCount) {
-    document.getElementById("test").innerHTML = answerTotal;
-} 
-else {
-    document.getElementById("test").innerHTML = answerTotal2;
-}
-
-}
-
+document.getElementById("test").innerHTML = answerTotal;
 
 //Awarding Points
 var studentResponse = 0;
@@ -53,17 +41,27 @@ if (studentResponse == randomWordCount) {
     score++;
 }
 //Page Refreshing
-
+/*
 var quizActive = 1;
 
-
-function checkQuiz() {
-    while (quizActive != 1) {
-        location.reload();
-    }
+while (quizActive != 1) {
+    location.reload()
 }
-<<<<<<< HEAD
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function changestring(){
     testText = document.getElementById("quiztext").value;
@@ -75,5 +73,3 @@ function testdisplay(teststring) {
     window.alert(teststring);
     return;
 }
-=======
->>>>>>> 84fab22b072cdb6c342c0bc130beb2dc63e3f5c5
