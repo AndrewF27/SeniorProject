4950af8a8c53ee7fd4281deff249a21a7ef23472
instilation00 <instilation00@gmail.com>
2021-03-02 15:04:18 -0800

    Scores page

--- Student Scores/showscore.php
+++ Student Scores/showscore.php
@@ -1,31 +1,43 @@
 <?php
-$FirstName = "FirstName";
-$LastName = "LastName";
-$Score = "Score";
-
 // Create connection
 $connection = new mysqli ("mysql.retentionapp.club","retentionadmin","R3t3n@dm1n");
 
 // Check connection
-if ($connection -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $connection -> connect_error;
-    exit();
+if ($connection === false) {
+    die("ERROR: Could not connect. " . $connection->connect_error);
 }
 
-//select database
 mysqli_select_db($connection,"retentionapp_login");
 
-$sql = "SELECT FirstName, LastName, Score FROM /*Database*/";
-
-$result = $conn->query($sql);
+$sql = "SELECT scoreID, studentName, quizID, score FROM student_scores";
 
 if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> Name: " . $row["firstname"]. " " . $row["lastname"] . " Score: " . $row["score"]. "<br>";
-    }
+    if($result = $connection->query($sql)){
+        if($result->num_rows > 0){
+            echo "<table>";
+                echo "<tr>";
+                    echo "<th>scoreID</th>";
+                    echo "<th>studentName</th>";
+                    echo "<th>quizID</th>";
+                    echo "<th>score</th>";
+                echo "</tr>";
+            while($row = $result->fetch_array()){
+                echo "<tr>";
+                    echo "<td>" . $row['scoreID'] . "</td>";
+                    echo "<td>" . $row['studentName'] . "</td>";
+                    echo "<td>" . $row['quizID'] . "</td>";
+                    echo "<td>" . $row['score'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            // Free result set
+            $result->free();
+        } else{
+            echo "No records matching your query were found.";
+        }
 } else {
-    echo "No results found for this name.";
+    echo "ERROR: Could not execute. Please try again $sql. " . $mysqli->error;
 }
 
+$connection->close();
 ?>
